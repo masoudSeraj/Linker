74357d48216b7c19b7b4924443e2db933750ab43
Masoud Seraj <masoud.seraj.1991@gmail.com>
2023-10-29 13:56:28 +0330

    wip

--- vite.config.js
+++ vite.config.js
@@ -35,23 +35,23 @@ export default defineConfig({
         description: "A simple link and query string creator",
         icons: [
           {
-            src: '/public/img/icon-192x192.png',
-            size: '192x192',
+            src: 'img/icon-192x192.png',
+            sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/public/img/icon-256x256.png',
-            size: '256x256',
+            src: 'img/icon-256x256.png',
+            sizes: '256x256',
             type: 'image/png'
           },
           {
-            src: '/public/img/icon-384x384.png',
-            size: '384x384',
+            src: 'img/icon-384x384.png',
+            sizes: '384x384',
             type: 'image/png'
           },
           {
-            src: '/public/img/icon-512x512.png',
-            size: '512x512',
+            src: 'img/icon-512x512.png',
+            sizes: '512x512',
             type: 'image/png'
           },
 
